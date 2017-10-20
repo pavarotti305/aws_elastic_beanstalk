a669edf10df997e4ec0b0583989bf1a93f3350c0
Lucien NTUMBA <admin@theartnewspaper.com>
2017-10-20 14:45:33 +0100

    use http module

--- roles/start_demo/files/app.js
+++ roles/start_demo/files/app.js
@@ -1,15 +1,9 @@
-// Load the express module.
-var express = require('express');
-var app = express();
-
-// Respond to requests for / with 'Hello World'.
-app.get('/', function(req, res){
+// Load the http module.
+var http = require('http');
+http.createServer(function(req, res) {
     res.setHeader("Content-Type", "text/html");
     res.write('<h2 style="color: blueviolet">Welcome Master Pava!</h2>');
     res.write('<p>Why I have taken so long to understand this stupid trick</p>')
     res.end();
-});
-
-// Listen on port 80 (like a true web server).
-app.listen(80);
-console.log('Express server started successfully.');
+}).listen(80);
+console.log('The http server have started successfully.');
